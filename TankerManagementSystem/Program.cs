181577a9ef0c273415aaf97e0a978e9afdebcd93
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2026-07-02 06:32:49 +0500

    Update Program.cs

--- TankerManagementSystem/Program.cs
+++ TankerManagementSystem/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<ApplicationDbContext>
 (options => options.UseSqlServer(configuration.GetConnectionString("conn")));
 
 
-// Adding Authentication old
+// Adding Authentication old new
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
