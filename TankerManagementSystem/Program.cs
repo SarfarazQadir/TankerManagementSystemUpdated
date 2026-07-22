0c1ffcff93d9f1cdc8106171e0f4d5b6829e2efe
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2026-07-22 06:15:14 +0500

    Update Program.cs

--- TankerManagementSystem/Program.cs
+++ TankerManagementSystem/Program.cs
@@ -11,7 +11,7 @@ using TankerManagementSystem.Services;
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
-// 1. DB Context New
+// 1. DB Context New old
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(configuration.GetConnectionString("conn")));
 
